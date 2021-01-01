1ef8e037c3a075f4c40d6340147a6b9c536f40e5
BitterOldMan2 <mikepattas211@gmail.com>
2021-01-01 15:14:58 -0100

    V1

---
+++ build/precache-manifest.74c676e5f75cde1299f32c6189ff3af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61afe916077cbabaa15ae07690ce5896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48b11303386457cdf67e",
+    "url": "/static/css/main.c672f238.chunk.css"
+  },
+  {
+    "revision": "a2b3d1a1ad98cd55bffc",
+    "url": "/static/js/2.a84e5db7.chunk.js"
+  },
+  {
+    "revision": "92bcffc488d6f3fbd12a7720ee4a3b28",
+    "url": "/static/js/2.a84e5db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b11303386457cdf67e",
+    "url": "/static/js/main.16a4bcae.chunk.js"
+  },
+  {
+    "revision": "e6448e7f8a8df7ca1283",
+    "url": "/static/js/runtime-main.9293aa72.js"
+  },
+  {
+    "revision": "c39308a53249528d605defed1df9aeb6",
+    "url": "/static/media/quiz-background.c39308a5.jpg"
+  }
+]);
