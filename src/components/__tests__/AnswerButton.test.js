5bc3c94c5ea04481fcd0632f210e1750912d420d
BitterOldMan2 <mikepattas211@gmail.com>
2021-03-27 10:33:15 -0100

    Test <AnswerButton/>

--- src/components/__tests__/AnswerButton.test.js
+++ src/components/__tests__/AnswerButton.test.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
+import Root from "../../Root";
 import AnswerButton from "../AnswerButton";
 import "../../setupTest";
 
-it("Renders a button element", () => {
-  const wrapper = shallow(<AnswerButton />);
-  expect(wrapper.find("button").length).toEqual(1);
+let wrapper;
+
+beforeEach(() => {
+  wrapper = mount(
+    <Root>
+      <AnswerButton answer="It works" />
+    </Root>
+  );
 });
+
+describe("<AnswerButton/>", () => {
+  it("Renders a button element", () => {
+    expect(wrapper.find("button").length).toEqual(1);
+  });
+
+  it("Has value equal to answer prop", () => {
+    expect(wrapper.find("button").text()).toEqual("It works");
+  });
+
+  it("Has a class name of answer-button", () => {
+    expect(wrapper.find("button").hasClass("answer-button")).toEqual(true);
+  });
+});
+
+/* 
+[x] Buttons value is equal to the answer prop
+[]  
+[]
+[]
+[]
+[]
+*/
