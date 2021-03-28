e5d9a9a3dfa789388c6b6ca0d9e58a653ace3521
BitterOldMan2 <mikepattas211@gmail.com>
2021-03-28 10:02:45 -0100

    Test App component

---
+++ src/__test__/App.test.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { mount } from "enzyme";
+import "../setupTest";
+import Root from "../Root";
+import App from "../App";
+import Navbar from "../components/Navbar";
+import GamePage from "../pages/GamePage";
+import Footer from "../components/Footer";
+import GameOver from "../components/GameOver";
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+});
+
+afterEach(() => {
+  wrapper.unmount();
+});
+
+describe("<App/>", () => {
+  it("Renders propertly", () => {
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it("Renders a <Navbar/>", () => {
+    expect(wrapper.containsMatchingElement(<Navbar />)).toEqual(true);
+  });
+
+  it("Reanders a <Footer/>", () => {
+    expect(wrapper.containsMatchingElement(<Footer />)).toEqual(true);
+  });
+
+  it("Reanders a <Footer/>", () => {
+    expect(wrapper.containsMatchingElement(<Footer />)).toEqual(true);
+  });
+});
+
+/* 
+[x] Component Renders Correctly
+[] Test switch statement
+[x] Renders NavBar 
+[x] Renders Footer
+
+ */
